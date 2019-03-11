128b65256db4f58c517a708fa3a4a570744e58a1
Alice Bjaaland <alicebjaaland@outlook.com>
2019-03-11 16:55:42 +0000

    Create hello.py and hellp.html files

---
+++ your_app/hello.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request
+
+app = Flask("MyApp")
+
+@app.route("/")
+def hello():
+    return "Hell) World"
+
+
+@app.route("/signup", methods=["POST", "GET"])
+def sign_up():
+    if request.method == 'POST':
+        form_data = request.form
+        print (form_data["email"])
+        return ("All Ok")
+    else:
+      return render_template('hello.html')
+
+app.run(port=5000)
+app.run(debug=True)
+# https://stackoverflow.com/questions/51700053/flask-werkzeug-exceptions-badrequestkeyerror
